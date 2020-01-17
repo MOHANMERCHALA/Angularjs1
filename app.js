62cb78123791df17c1dc067d4675bb7e3dc73c12
mohan mecherla <mohankrishnann601@gmail.com>
2020-01-17 11:13:07 +0530

    Exercise2

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 (function (){
-
-'use strict';
-
-angular.module('myFirstApp', [])
-
-.controller('MyFirstController', function ($scope) {
-$scope.name="Kuberan";
-$scope.sayhello=function(){
-    return "Hello I am angular!"
-}
-
-});
-})();
+    'use strict';
+    angular.module('namecal', [])
+    .controller('namecalcontroller',function($scope){
+        $scope.name="";
+        $scope.totalval=0;
+        $scope.displayname=function(){
+            var totalname=calcval($scope.name);
+            $scope.totalval=totalname;
+        };
+        function calcval(string){
+            var totalstr=0;
+            for(var i=0;i<string.length;i++){
+                totalstr+=string.charCodeAt(i);
+            }
+                return totalstr;
+            }
+        });
+  })
+();
